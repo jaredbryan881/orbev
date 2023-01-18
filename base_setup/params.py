3a5048c5d03982c1f003968c650c3ab2d1cf19b4
jaredbryan881 <jared.bryan1998@gmail.com>
2023-01-18 16:03:56 -0500

    Consider initial time t0 instead of an additional profile number, preparing for partial profile caching...

--- base_setup/params.py
+++ base_setup/params.py
@@ -3,15 +3,17 @@ import numpy as np
 ###########
 # Toggles #
 ###########
-live_orbit=False
-time_reversed=False
+live_orbit=False # evolve the orbital configuration?
+time_reversed=False # evolve time backward?
+store_all_profiles=False # store every single profile, or only those close to the current time?
 
 #################################
 # Initial Orbital Configuration #
 #################################
-e0 = np.array([0.5]*50)
-OmegaOrb0 = np.array([0.1774]*50)
-OmegaRot0 = np.array([0.1774]*50)*4.8
+t0=5e8 # [yr]
+e0=np.array([0.5]*50) # []
+OmegaOrb0=np.array([0.1774]*50) # [cyc/day]
+OmegaRot0=np.array([0.1774]*50)*4.8 # [cyc/day]
 
 #################
 # Update limits #
@@ -25,11 +27,11 @@ max_dOmegaRot=1e-5 # [cyc/day]
 # Paths and Filenames #
 #######################
 gyre_inlist="./gyre_tides.in"
-#gyre_inlist="./gyre_orbit.in"
 mesa_diname="/home/jared/MIT/astero/mesa_HATP2/live_planet"
 
 # subset of profiles which can be selected from
 allowable_profiles=np.arange(1,1001,10)
+n_profiles=50
 
 def main():
 	import sys
@@ -37,6 +39,7 @@ def main():
 	print("####################################")
 	print("#### Initial Orbital Parameters ####")
 	print("####################################")
+	print("t_0: {} Gyr".format(t0/1e9))
 	print("e_0: {}".format(e0[ip_ind]))
 	print("Omega_orb0: {} cyc/day".format(OmegaOrb0[ip_ind]))
 	print("Omega_rot0: {} cyc/day".format(OmegaRot0[ip_ind]))
